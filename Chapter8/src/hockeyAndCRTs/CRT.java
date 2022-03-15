22f90f620d7b4ba1a2dd187dd898f0c6780bc8e2
105128004 <105128004@L815WD150040.edu.cbe.ab.ca>
2022-03-15 10:01:44 -0600

    Chapter 8 update

---
+++ Chapter8/src/hockeyAndCRTs/CRT.java
@@ -0,0 +1,20 @@
+1) a has-a relationship describes a class containing another class while
+an is-a relationship describes a class becoming an extension of an existing
+class
+
+2) The word public, then the name of the class, then brackets containing parameters
+eg. public test(a, b)
+{
+
+}
+
+3) public modifier means it can be accessed by other classes while the private
+modifier means it can only be accessed within the class
+
+4) assuming the "2" given to the dot describes the radius of the dot, then the
+statement is invalid because you can not change the radius of the dot from
+the testing class without calling on a method such as setRadius()
+
+6) An object is a class but a class is not always an object. A class will often
+describe an object however a class can also be the testing class which produces
+no object.
